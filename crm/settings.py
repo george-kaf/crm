f887dc1c9236673bc62db2325754c7b2b36b8b8b
george-kaf <giorgoskafetzo@gmail.com>
2024-03-24 22:26:26 +0000

    added logo in navbar and in images

--- crm/settings.py
+++ crm/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
